791cbab500fcb15de9187c88f25fc257d90152a5
raghunandan68 <nallanraghu20@gmail.com>
2025-04-11 14:19:51 +0530

    Update CalculateTest.java

--- Junit/CalculateTest.java
+++ Junit/CalculateTest.java
@@ -2,15 +2,15 @@ import static.org.junit.Assert.*;
 import org.junit.Test;
 
 class Calculate{
-    public int subtract(int var1,int var2){
+    public int sum(int var1,int var2){
         System.out.println("Adding values : "+var1+" + "+var2);
-        return var1-var2;
+        return var1+var2;
     }
 }
 
 public class CalculateTest{
     Calculate cal=new Calculate();
-    int sum=cal.subtract(10,5);
+    int sum=cal.sum(10,5);
     int testsum=15;
     @Test
     public void testSum(){
