c6e46d5fea7ada05ce9a2062f2f08b2209e4dc75
raghunandan68 <nallanraghu20@gmail.com>
2025-04-24 21:41:04 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,11 @@
+public class Student {
+    String id;
+    String name;
+    String biometricId;
+
+    public Student(String id, String name, String biometricId) {
+        this.id = id;
+        this.name = name;
+        this.biometricId = biometricId;
+    }
+}
